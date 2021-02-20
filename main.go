5af20432a8e4e60d402989c81c0fb2698b463d0d
gbaranski <gbaranski19@gmail.com>
2021-02-20 16:48:39 +0100

    feat: impove measuring distance

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/agnivade/levenshtein"
 	"github.com/bwmarrin/discordgo"
 	"github.com/fhodun/stupid-questions/config"
 	log "github.com/sirupsen/logrus"
-	//"strconv"
-	"strings"
 )
 
 var cfg = config.GetConfig()
@@ -31,27 +32,83 @@ func main() {
 	select {}
 }
 
+type CancerWordTag struct {
+	Weight uint
+	Text   string
+}
+
+// CancerWord is used for defining cancer words
+type CancerWord struct {
+	PrimaryText string
+	Tags        []CancerWordTag
+	Answer      string
+}
+
+const MinWeight uint = 5
+const MaxDistance int = 2
+
+func SmartStringContains(str string, substr string) bool {
+	for i := 0; i < len(str)-len(substr); i++ {
+		if levenshtein.ComputeDistance(substr, str[i:i+len(substr)]) < MaxDistance {
+			return true
+		}
+	}
+
+	return false
+}
+
+func GetMessageCancer(str string, cancerWords []CancerWord) *CancerWord {
+	words := strings.Split(str, " ")
+
+	for _, cw := range cancerWords {
+		if !SmartStringContains(str, cw.PrimaryText) {
+			continue
+		}
+
+		w := uint(0)
+		hasAnyTag := false
+
+		for _, word := range words {
+			if word == cw.PrimaryText {
+				continue
+			}
+			for _, tag := range cw.Tags {
+				fmt.Printf("distance: %d from %s to %s\n", levenshtein.ComputeDistance(word, tag.Text), word, tag.Text)
+				if distance := levenshtein.ComputeDistance(word, tag.Text); distance < MaxDistance {
+					hasAnyTag = true
+					w += tag.Weight
+					if w > MinWeight {
+						return &cw
+					}
+				}
+			}
+		}
+		if !hasAnyTag {
+			return nil
+		}
+		fmt.Printf("w: %d\n", w)
+	}
+	return nil
+}
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	cancerWords := []string{ "opera", "testportal" }
-	content := m.Content
-	split := strings.Split(content, " ")
-	// TODO: add punctuation characters deletion e.g. '.' and ','
-	var words []string
-	println("Content words number: ", len(split))
-	for i := 0; i < len(split); i++ {
-		println("\nTESTED WORD: ", split[i])
-		for j := 0; j < len(cancerWords); j++ {
-			println("Searched word: ", cancerWords[j])
-			distance := levenshtein.ComputeDistance(split[i], cancerWords[j])
-			println("Difference: ", distance)
-			if distance <= cfg.DistanceMax {
-				words = append(words, split[i])
-			}
-		}
+	cancerWords := []CancerWord{
+		{
+			PrimaryText: "anti-testportal",
+			Answer:      "tak kurwa dziala spierdalaj",
+			Tags: []CancerWordTag{
+				{
+					Weight: 10,
+					Text:   "dziala",
+				},
+			},
+		},
 	}
-	//println(words)
+	if cw := GetMessageCancer(m.Content, cancerWords); cw != nil {
+		s.ChannelMessageSendReply(m.ChannelID, cw.Answer, m.Reference())
+	}
 }
